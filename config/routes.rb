b6428541a63c5ee683f714f5ca5699f702071c41
ivke <ivked85@gmail.com>
2018-02-17 00:17:57 +0000

    requiring authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   
-  resources :invoices
-  resources :invoice_templates
-
+  authenticate :user do
+    resources :invoices
+    resources :invoice_templates
+  end
 end
